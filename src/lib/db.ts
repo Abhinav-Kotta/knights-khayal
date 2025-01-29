2d335be8e530e109234073db023db08e2122f876
Abhinav-Kotta <abhinav.kotta@gmail.com>
2025-01-29 13:12:21 -0500

    this better work

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-interface CustomPrismaClient extends PrismaClient {
-  $use(arg: any): any
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-declare global {
-  var prisma: CustomPrismaClient | undefined
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
-export const prisma = global.prisma || new PrismaClient() as CustomPrismaClient
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
